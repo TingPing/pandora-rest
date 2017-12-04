95c3e959e68cffe07bc9a2db1b58b7521f2471c0
Patrick Griffis <tingping@tingping.se>
2017-12-04 09:58:01 -0500

    Add some missing type annotations

--- pandora.py
+++ pandora.py
@@ -7,14 +7,14 @@ class Client:
         self._auth_token = ''
         self._csrf_token = ''
 
-    async def _get_csrf_token(self):
+    async def _get_csrf_token(self) -> str:
         uri = 'https://www.pandora.com'
         message = soupy.Message.new_for_uri(soupy.Method.HEAD, uri)
         response = await self._session.send_message(message)
         self._session.save_cookies_from_response(response, uri)
         return self._session.cookies['csrftoken']
 
-    async def _send_message(self, method: str, body: str):
+    async def _send_message(self, method: str, body: str) -> dict:
         uri = 'https://www.pandora.com/api/v1/' + method
         message = soupy.Message.new_for_uri(soupy.Method.POST, uri)
         message.add_header('X-CsrfToken', self._csrf_token)
@@ -24,7 +24,7 @@ class Client:
         response = await self._session.send_message(message)
         return response.get_json_body()
 
-    async def login(self, email: str, password: str):
+    async def login(self, email: str, password: str) -> dict:
         self._csrf_token = await self._get_csrf_token()
 
         response = await self._send_message('auth/login', {
