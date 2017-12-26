626af34558c4d6a278555baaa2e9bfa6391472c1
Patrick Griffis <tingping@tingping.se>
2017-12-26 09:06:51 -0500

    Add another test

--- tests/test_soupy.py
+++ tests/test_soupy.py
@@ -9,3 +9,7 @@ async def test_session():
     ret = await session.get('https://httpbin.org/get')
     assert ret.headers['Content-Type'] == 'application/json'
     assert ret.json_body['url'] == 'https://httpbin.org/get'
+
+    body = {'key': 'val'}
+    ret = await session.post('https://httpbin.org/post', body)
+    assert ret.json_body['json'] == body
